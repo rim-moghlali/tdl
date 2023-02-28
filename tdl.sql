cd098c2353ad02c38a05a36feff1e81b9eee822a
rim-moghlali <rim-moghlali@laplateforme.io>
2023-02-28 18:05:13 +0100

    update sql database

--- tdl.sql
+++ tdl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3307
--- Généré le : mar. 28 fév. 2023 à 16:46
+-- Généré le : mar. 28 fév. 2023 à 17:04
 -- Version du serveur : 10.6.5-MariaDB
 -- Version de PHP : 7.4.26
 
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `user_id`, `todo_list_id`, `description`, `completed`, `completed_at`) VALUES
-(25, 3, 15, 'finish school', 0, NULL);
+(25, 3, 15, 'finish school', 0, '2023-02-28 17:59:41');
 
 -- --------------------------------------------------------
 
@@ -61,14 +61,15 @@ CREATE TABLE IF NOT EXISTS `todo_lists` (
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `todo_lists`
 --
 
 INSERT INTO `todo_lists` (`id`, `user_id`, `title`, `created_at`) VALUES
-(15, 3, 'school', '2023-02-28 17:38:10');
+(15, 3, 'school', '2023-02-28 17:38:10'),
+(17, 3, 'kjdf', '2023-02-28 18:00:50');
 
 -- --------------------------------------------------------
 
@@ -85,7 +86,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `lastname` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `users`
@@ -93,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `login`, `password`, `firstname`, `lastname`, `email`) VALUES
 (3, 'rim', '$2y$10$zcM/czt/voShRVJm5/r.2uouXw8V5WrStCnhCk9DiCmjsvgJYuVFm', 'Rim', 'Moghlali', 'rim.moghlali@laplateforme.io'),
-(2, 'abrahamukachi', '$2y$10$0bYmhH1jZonp6fwU.gv3V.WY1WICkrl2MorAAfBnLBRKn1tInFvIm', 'Abraham', 'Ukachi', 'abraham.ukachi@laplateforme.io');
+(4, 'admin', '$2y$10$.kn/1rL73Tb9830uHbTrQebqN5pfCX6FFHOj0S3cVf2VQmx1nG0yS', 'Admin', 'Admin', 'admin@admin.com');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
